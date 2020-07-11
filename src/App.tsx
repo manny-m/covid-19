7040efb9fb7f2fd191863617d090e91be21d0135
manny-m <savage.kappa@gmail.com>
2020-07-11 13:01:43 -0400

    basic set up

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,55 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import { CssBaseline } from '@material-ui/core';
+
+import neatCsv from 'neat-csv';
+
+const primaryColor = '#1769aa';
+const secondaryColor = '#aa4d17';
+const errorColor = '#b2483e';
+const warnColor = '#FFCF44';
+const infoColor = '#3491bb';
+const successColor = '#00a152';
+
+const customTheme = createMuiTheme({
+  palette: {
+    primary:{ main: primaryColor },
+    secondary:{ main: secondaryColor },
+    error:{ main: errorColor },
+    warning:{ main: warnColor },
+    info:{ main: infoColor },
+    success:{ main: successColor },
+    type:'dark'
+  }
+});
+
 
 function App() {
+
+  React.useEffect(()=>{
+    const getFile = async () =>{
+      try{
+        const csv = await fetch('https://raw.githubusercontent.com/nytimes/covid-19-data/master/us.csv')
+          .then((response)=>{ return response.text() })
+          .then((rawCSV)=>{ return neatCsv(rawCSV) });
+        console.log(csv);
+        
+      }catch(error){
+        console.error('There was an issue initializing ');
+      }
+    }
+
+    getFile();
+
+  },[]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MuiThemeProvider theme={customTheme}>
+      <CssBaseline/>
+      <div>
+        test
+      </div>
+    </MuiThemeProvider>
   );
 }
 
